5a38aebd38dd5899f660b0feb2f4b894890cf923
juniorasmat22 <juniorasmat40@gmail.com>
2020-09-30 23:05:47 -0500

    agrgando la estructura

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const path=require('path');
 const express= require('express');
 const app=express();
 const SocketIO=require('socket.io');
+const indexRouter=require('./routes/room');
 //setting
 app.set('port',process.env.PORT||3030);
 
+app.use(indexRouter);
+app.set('view engine','ejs')
 //STATIC FILES
 app.use(express.static(path.join(__dirname,'public')));
 
