6f1e955f76f85b373590d3e1a101efc930fc96c4
juniorasmat22 <juniorasmat40@gmail.com>
2020-10-10 22:29:42 -0500

    scripts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const express= require('express');
 const app=express();
 const SocketIO=require('socket.io');
 const indexRouter=require('./routes/room');
-const { Socket } = require('dgram');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 //setting
 app.set('port',process.env.PORT||3030);
 app.set('views',path.join(__dirname,'views'));
@@ -11,7 +14,7 @@ app.use(indexRouter);
 app.set('view engine','ejs');
 //STATIC FILES
 app.use(express.static(path.join(__dirname,'public')));
-
+app.use('/peerjs', peerServer);
 //listen server
 const server=app.listen(app.get('port'),()=>{
     console.log("server on port",app.get('port'));
